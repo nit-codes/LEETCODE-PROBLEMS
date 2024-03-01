4317213d406f832cb868e734cdb6a869b3017930
NITHIN B Y <137317447+nit-codes@users.noreply.github.com>
2024-03-01 20:22:16 +0530

    Create maximum odd binary number.java

---
+++ maximum odd binary number.java
@@ -0,0 +1,26 @@
+public class Solution {
+    public String maximumOddBinaryNumber(String s) {
+        int n = s.length();
+        
+        // Initialize result string with '0' of length n
+        StringBuilder result = new StringBuilder();
+        for (int k = 0; k < n; k++) {
+            result.append('0');
+        }
+        
+        int i = 0;
+        
+        for (char ch : s.toCharArray()) {
+            if (ch == '1') {
+                if (result.charAt(n - 1) == '1') {
+                    result.setCharAt(i, '1');
+                    i++;
+                } else {
+                    result.setCharAt(n - 1, '1');
+                }
+            }
+        }
+        
+        return result.toString();
+    }
+}
