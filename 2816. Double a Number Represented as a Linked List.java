c46d80b51abb4b01646292e110b96b7fc78e8ae5
NITHIN B Y <137317447+nit-codes@users.noreply.github.com>
2024-05-07 19:45:01 +0530

    Create 2816. Double a Number Represented as a Linked List.java

---
+++ 2816. Double a Number Represented as a Linked List.java
@@ -0,0 +1,42 @@
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        
+        ListNode last = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        
+        return last;
+    }
+    
+    public ListNode doubleIt(ListNode head) {
+        head = reverseList(head);
+        
+        ListNode curr = head;
+        ListNode prev = null;
+        int carry = 0;
+        
+        while (curr != null) {
+            int newValue = curr.val * 2 + carry;
+            curr.val = newValue % 10;
+            
+            if (newValue >= 10) {
+                carry = 1;
+            } else {
+                carry = 0;
+            }
+            
+            prev = curr;
+            curr = curr.next;
+        }
+        
+        if (carry != 0) {
+            ListNode newHead = new ListNode(carry);
+            prev.next = newHead;
+        }
+        
+        return reverseList(head);
+    }
+}
